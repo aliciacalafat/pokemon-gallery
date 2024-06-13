8b6ebd5a72c2871b938c2bd9b628dbbd9ce2933a
Alicia Calafat <aliciacalafat@gmail.com>
2024-06-13 03:39:58 +0200

    Register service worker and update base href for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,18 @@ import { importProvidersFrom } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js')
+      .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
